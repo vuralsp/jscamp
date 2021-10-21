07a9490d3e8eee0a5a56fa689de133479ee9c7b1
email.vural@gmail.com <email.vural@gmail.com>
2021-10-21 20:09:11 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,27 @@
+console.log("Merhaba Kodlama.io")
+
+
+let dolarBugun = 9.30
+let dolarDun = 9.20
+
+{
+    let dolarDun = 9.10
+}
+console.log(dolarDun)
+
+const euroDun = 11.20
+//euroDun = 11// constla tanımlanan bir değişkenin değeri sabittir
+
+console.log(euroDun)
+
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+console.log("<ul>")
+
+for (let i=0;i<konutKredileri.length;i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")   
+}
+
+console.log("</ul>")
+
+console.log(konutKredileri)
